1cfa19c48ef1f5fc8b28cfa22a80f7dbfbe8f8df
Chase Ingebritson <chase.ingebritson2014@gmail.com>
2020-03-01 01:09:06 -0600

    Added a few boxes and a skybox

--- src/components/Viewer.vue
+++ src/components/Viewer.vue
@@ -10,23 +10,24 @@ import { Component, Vue } from 'vue-property-decorator'
 import { Engine } from '@babylonjs/core/Engines/engine'
 import { Scene } from '@babylonjs/core/scene'
 import { Vector3 } from '@babylonjs/core/Maths/math'
-import { UniversalCamera } from '@babylonjs/core/Cameras/universalCamera'
+import { FreeCamera } from '@babylonjs/core/Cameras/freeCamera'
 import { HemisphericLight } from '@babylonjs/core/Lights/hemisphericLight'
 import '@babylonjs/core/Loading/loadingScreen'
 import { AssetsManager, MeshAssetTask } from '@babylonjs/core/Misc/assetsManager'
 import { MeshBuilder } from '@babylonjs/core/Meshes/meshBuilder'
 import '@babylonjs/core/Collisions/collisionCoordinator'
+import { StandardMaterial } from '@babylonjs/core/Materials/standardMaterial'
+import { ActionManager, ExecuteCodeAction, ActionEvent } from '@babylonjs/core/Actions'
 
-import { NormalMaterial } from '@babylonjs/materials/normal'
-
-import '@babylonjs/loaders'
+// import '@babylonjs/loaders'
+import { SkyMaterial } from '@babylonjs/materials/sky/skyMaterial'
 
 @Component
 export default class Viewer extends Vue {
   private canvas!: HTMLCanvasElement
   private engine!: Engine
   private scene!: Scene
-  private camera!: UniversalCamera
+  private camera!: FreeCamera
   private assetsManager!: AssetsManager
 
   private async mounted () {
@@ -35,17 +36,16 @@ export default class Viewer extends Vue {
     this.engine = new Engine(this.canvas)
 
     this.scene = new Scene(this.engine)
-    this.scene.gravity = new Vector3(0, -9.81, 0)
+    this.scene.gravity = new Vector3(0, -1, 0)
     this.scene.collisionsEnabled = true
 
-    this.camera = new UniversalCamera('camera1', new Vector3(0, 10, 0), this.scene)
-    this.camera.setTarget(Vector3.Zero())
+    this.camera = new FreeCamera('camera', new Vector3(0, 2, 0), this.scene)
     this.camera.attachControl(this.canvas, true)
     this.camera.ellipsoid = new Vector3(1, 1, 1)
     this.camera.checkCollisions = true
     this.camera.applyGravity = true
 
-    const light = new HemisphericLight('light1', new Vector3(0, 1, 0), this.scene)
+    const light = new HemisphericLight('hemiLight', new Vector3(0, 1, 0), this.scene)
     light.intensity = 0.7
 
     // this.assetsManager = new AssetsManager(this.scene)
@@ -58,10 +58,41 @@ export default class Viewer extends Vue {
     //
     // this.assetsManager.load()
 
-    const ground = MeshBuilder.CreateGround('ground', { width: 50, height: 50, subdivisions: 10 }, this.scene)
-    ground.material = new NormalMaterial('groundMat', this.scene)
+    // https://doc.babylonjs.com/extensions/sky
+    const skybox = MeshBuilder.CreateBox('skybox', { size: 1000 }, this.scene)
+    const skyMaterial = new SkyMaterial('skyMaterial', this.scene)
+    skyMaterial.backFaceCulling = false
+    skybox.material = skyMaterial
+
+    const ground = MeshBuilder.CreateGround('ground', { width: 100, height: 100, subdivisions: 10 }, this.scene)
+    ground.material = new StandardMaterial('groundMat', this.scene)
     ground.material.backFaceCulling = false
     ground.checkCollisions = true
+
+    const menuBoxes = [
+      { name: 'box1', position: new Vector3(-2, 1, -5), url: 'https://www.google.com/' },
+      { name: 'box2', position: new Vector3(0, 1, -5), url: 'https://www.google.com/' },
+      { name: 'box3', position: new Vector3(2, 1, -5), url: 'https://www.google.com/' },
+      { name: 'box4', position: new Vector3(-2, 3, -5), url: 'https://www.google.com/' },
+      { name: 'box5', position: new Vector3(0, 3, -5), url: 'https://www.google.com/' },
+      { name: 'box6', position: new Vector3(2, 3, -5), url: 'https://www.google.com/' }
+    ]
+
+    menuBoxes.forEach(box => {
+      const mesh = MeshBuilder.CreateBox(box.name, { size: 1 }, this.scene)
+      mesh.checkCollisions = true
+      mesh.position = box.position
+      mesh.actionManager = new ActionManager(this.scene)
+
+      mesh.actionManager.registerAction(
+        new ExecuteCodeAction(ActionManager.OnPickTrigger, (event) => {
+          this.onMenuBoxClick(event, box.url)
+        })
+      )
+    })
+
+    const target = new Vector3(0, 2, -5)
+    this.camera.setTarget(target)
 
     this.engine.runRenderLoop(() => {
       this.scene.render()
@@ -78,6 +109,11 @@ export default class Viewer extends Vue {
         reject(task)
       }
     })
+  }
+
+  private onMenuBoxClick (event: ActionEvent, url: string) {
+    console.log('onclick')
+    window.open(url)
   }
 }
 </script>
