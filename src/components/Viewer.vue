71c442d30341d587a1831f33288bf5e19c131734
Chase Ingebritson <chase.ingebritson2014@gmail.com>
2021-02-24 17:04:57 -0600

    Added debug layer, fixed physics

--- src/components/Viewer.vue
+++ src/components/Viewer.vue
@@ -27,6 +27,8 @@ import {
 } from '@babylonjs/core'
 import '@babylonjs/core/Loading/loadingScreen'
 import '@babylonjs/core/Collisions/collisionCoordinator'
+import '@babylonjs/core/Debug/debugLayer'
+import '@babylonjs/inspector'
 
 // import '@babylonjs/loaders'
 import { SkyMaterial } from '@babylonjs/materials/sky/skyMaterial'
@@ -37,15 +39,16 @@ export default class Viewer extends Vue {
   private scene!: Scene
   private camera!: FreeCamera
   private assetsManager!: AssetsManager
+  private canvas!: HTMLCanvasElement
 
   private async mounted () {
-    const canvas = this.$refs.canvas as HTMLCanvasElement
-    this.engine = new Engine(canvas)
+    this.canvas = this.$refs.canvas as HTMLCanvasElement
+    this.engine = new Engine(this.canvas)
 
     this.scene = new Scene(this.engine)
 
     this.camera = new FreeCamera('camera', new Vector3(0, 2, 0), this.scene)
-    this.camera.attachControl(canvas, true)
+    this.camera.attachControl(this.canvas, true)
     this.camera.ellipsoid = new Vector3(1, 1, 1)
 
     const light = new HemisphericLight('hemiLight', new Vector3(0, 1, 0), this.scene)
@@ -79,12 +82,15 @@ export default class Viewer extends Vue {
     ground.physicsImpostor = new PhysicsImpostor(ground, PhysicsImpostor.BoxImpostor, { mass: 0, friction: 0.5, restitution: 0.7 }, this.scene)
 
     const menuBoxes = [
-      { name: 'box1', position: new Vector3(-2, 1, -5), url: 'https://www.google.com/', physicsEnabled: true, texture: '../assets/TestCactus1.png' },
-      { name: 'box2', position: new Vector3(0, 1, -5), url: 'https://www.google.com/', physicsEnabled: true, texture: 'assets/TestCactus2.png' },
-      { name: 'box3', position: new Vector3(2, 1, -5), url: 'https://www.google.com/', physicsEnabled: true, texture: 'assets/TestCactus3.png' },
-      { name: 'box4', position: new Vector3(-2, 3, -5), url: 'https://www.google.com/', physicsEnabled: false, texture: 'assets/TestCactus1.png' },
-      { name: 'box5', position: new Vector3(0, 3, -5), url: 'https://www.google.com/', physicsEnabled: false, texture: 'assets/TestCactus2.png' },
-      { name: 'box6', position: new Vector3(2, 3, -5), url: 'https://www.google.com/', physicsEnabled: false, texture: 'assets/TestCactus3.png' }
+      { name: 'box1', position: new Vector3(-1.1, 1, -5), url: 'https://www.google.com/', physicsEnabled: false, texture: '../assets/TestCactus1.png' },
+      { name: 'box2', position: new Vector3(0, 1, -5), url: 'https://www.google.com/', physicsEnabled: false, texture: 'assets/TestCactus2.png' },
+      { name: 'box3', position: new Vector3(1.1, 1, -5), url: 'https://www.google.com/', physicsEnabled: false, texture: 'assets/TestCactus3.png' },
+      { name: 'box4', position: new Vector3(-1.1, 2.1, -5), url: 'https://www.google.com/', physicsEnabled: false, texture: 'assets/TestCactus1.png' },
+      { name: 'box5', position: new Vector3(0, 2.1, -5), url: 'https://www.google.com/', physicsEnabled: false, texture: 'assets/TestCactus2.png' },
+      { name: 'box6', position: new Vector3(1.1, 2.1, -5), url: 'https://www.google.com/', physicsEnabled: false, texture: 'assets/TestCactus3.png' },
+      { name: 'box7', position: new Vector3(-1.1, 3.2, -5), url: 'https://www.google.com/', physicsEnabled: false, texture: 'assets/TestCactus3.png' },
+      { name: 'box8', position: new Vector3(0, 3.2, -5), url: 'https://www.google.com/', physicsEnabled: false, texture: 'assets/TestCactus3.png' },
+      { name: 'box9', position: new Vector3(1.1, 3.2, -5), url: 'https://www.google.com/', physicsEnabled: false, texture: 'assets/TestCactus3.png' }
     ]
 
     menuBoxes.forEach(box => {
@@ -99,9 +105,8 @@ export default class Viewer extends Vue {
         })
       )
 
-      if (box.physicsEnabled) {
-        mesh.physicsImpostor = new PhysicsImpostor(mesh, PhysicsImpostor.BoxImpostor, { mass: 3 }, this.scene)
-      }
+      mesh.physicsImpostor = new PhysicsImpostor(mesh, PhysicsImpostor.BoxImpostor, { mass: 3, friction: 1, restitution: 0 }, this.scene)
+      mesh.physicsImpostor.sleep()
 
       const meshMaterial = new StandardMaterial(box.name + '_material', this.scene)
       meshMaterial.diffuseTexture = new Texture(box.texture, this.scene)
@@ -116,6 +121,10 @@ export default class Viewer extends Vue {
     this.engine.runRenderLoop(() => {
       this.scene.render()
     })
+
+    this.scene.debugLayer.show()
+
+    this.$emit('ready', this.scene)
   }
 
   private loadAsset (taskName: string, url: string) {
@@ -131,20 +140,18 @@ export default class Viewer extends Vue {
   }
 
   private onMenuBoxClick (mesh: Mesh, event: ActionEvent, url: string) {
-    console.log('onMenuBoxClick', event)
+    if (!mesh.physicsImpostor) return
 
-    if (!mesh.physicsImpostor) {
-      mesh.physicsImpostor = new PhysicsImpostor(mesh, PhysicsImpostor.BoxImpostor, { mass: 3 }, this.scene)
-    } else {
-      // Select the mesh again using a scene pick because apparently I can't get this info via the event...
-      const pick = this.scene.pick(event.pointerX, event.pointerY)
-      if (pick && pick.getNormal()) {
-        console.log(pick.getNormal())
+    mesh.physicsImpostor.wakeUp()
 
-        const impulseDirection = pick.getNormal()
-        const impulseMagnitude = 5
-        const contactLocalRefPoint = Vector3.Zero()
+    // Select the mesh again using a scene pick because apparently I can't get this info via the event...
+    const pick = this.scene.pick(event.pointerX, event.pointerY)
+    if (pick?.ray?.direction) {
+      const impulseDirection = pick.ray.direction
+      const impulseMagnitude = 7
+      const contactLocalRefPoint = Vector3.Zero()
 
+      if (impulseDirection) {
         mesh.physicsImpostor.applyImpulse(
           impulseDirection.scale(impulseMagnitude),
           mesh.getAbsolutePosition().add(contactLocalRefPoint)
